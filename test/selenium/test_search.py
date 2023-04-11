bc75b6765c4eab7666fa71ccc57ba7109234b778
Emily1105 <130475668+Emily1105@users.noreply.github.com>
2023-04-12 00:21:43 +0800

    Update test_search.py

--- test/selenium/test_search.py
+++ test/selenium/test_search.py
@@ -1,19 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from time import sleep
 
 class TestSearch:
     def setup(self):
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
-        self.driver.implicitly_wait(3)
+        self.driver.implicitly_wait(5)
 
     def teardown(self):
         self.driver.quit()
 
     def test_search(self):
         self.driver.get("https://www.lagou.com/")
+        self.driver.find_element(By.ID,"cboxClose").click()
+        sleep(3)
         self.driver.find_element(By.ID,"search_input").send_keys("华为")
-        self.driver.find_element(By.ID, "search_button").clear()
+        self.driver.find_element(By.ID, "search_button").click()
         ele = self.driver.find_element(By.CSS_SELECTOR,".cl-right-top__1cCMk h3")
-        print(ele.text)
+        assert "华为技术有限公司" == ele.text
